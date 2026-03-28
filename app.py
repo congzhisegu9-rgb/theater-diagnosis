48123c57f3ac595b1675153f625a8266faa06a48
congzhisegu9-rgb <congzhisegu9@gmail.com>
2026-03-28 12:54:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -197,12 +197,12 @@ def set_bg(image_file):
 
     /* ===== 中央寄せ ===== */
     .choice-wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 60vh;
-}
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 60vh;
+    }
 
     /* ===== ボタン（超デカ文字） ===== */
     div.stButton {{
